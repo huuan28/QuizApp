5a521c17adbf2c87121a4a573296c925ac40755b
huuan28 <22dh110024@st.huflit.edu.vn>
2024-02-26 00:12:59 +0700

    update 26-2

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.example.quizapp.model.User;
 
 public class MainActivity extends AppCompatActivity {
+    User user;
+    Intent intent;
 
+    ImageView avt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Init();
     }
+
+    private void Init() {
+        avt = findViewById(R.id.avt);
+        Glide.with(this)
+                .load(R.drawable.base_avt)
+                .circleCrop()
+                .into(avt);
+    }
+
 }
